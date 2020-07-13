fe80215bb9b10f2656772fcbd1b93e587ad47f79
Rag Dhiman <rag.dhiman@gmail.com>
2020-07-13 13:26:21 +0100

    Update dueText.rb

--- duesummary-1.0.0/lib/due/dueText.rb
+++ duesummary-1.0.0/lib/due/dueText.rb
@@ -2,70 +2,71 @@
 module Due
   # The dueText class provides all the text for the due-summary functionality.
   class DueText
+    # Method provides text for minutes measure name.
     def self.minutes
       'Minutes'
     end
-
+    # Method provides text for hours measure name.
     def self.hours
       'Hours'
     end 
-
+    # Method provides text for days measure name.
     def self.days
       'Days'
     end 
-
+    # Method provides text for weeks measure name.
     def self.weeks
       'Weeks'
     end 
-
+    # Method provides text for months measure name.
     def self.months
       'Months'
     end 
-
+    # Method provides text for years measure name.
     def self.years
       'Years'
     end 
-
-    def self.minutes_prefix
+    # Method provides text for minutes suffix for a messure.
+    def self.minutes_suffix
       'min(s)'
     end
-
-    def self.hours_prefix
+    # Method provides text for hours suffix for a messure.
+    def self.hours_suffix
       'hours(s)'
     end 
-
-    def self.days_prefix
+    # Method provides text for days suffix for a messure.
+    def self.days_suffix
       'day(s)'
     end 
-
-    def self.weeks_prefix
+    # Method provides text for weeks suffix for a messure.
+    def self.weeks_suffix
       'week(s)'
     end 
-
-    def self.months_prefix
+    # Method provides text for months suffix for a messure.
+    def self.months_suffix
       'month(s)'
     end 
-
-    def self.years_prefix
+    # Method provides text for years suffix for a messure.
+    def self.years_suffix
       'year(s)'
     end
-
+    # Method provides text for period.
     def self.period
       'Period'
     end
-
+    # Method provides text for duration.
     def self.duration
       'Duration'
     end 
-
+    # Method provides text for measure.
     def self.measure
       'Measure'
     end 
-
+    # Method provides text for since.
     def self.since
       'since'
     end 
-
+    # Method provides text for till.
     def self.till
       'till'
     end 
